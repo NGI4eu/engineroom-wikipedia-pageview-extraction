d9842dd1f933abb7bae87786fe659d46d9b540ca
CristianCantoro <kikkocristian@gmail.com>
2017-05-08 12:18:52 +0200

    Search regexes from simplified redirects files

--- extract_data.sh
+++ extract_data.sh
@@ -19,6 +19,7 @@ outputdir=''
 lang=''
 dry_run=''
 restart=false
+prefix=''
 read -rd '' docstring <<EOF
 Usage:
   extract_data.sh [options] -f INFILE -y YEARMONTH -g GZDIR
@@ -30,13 +31,12 @@ Usage:
     --datadir DATADIR           Data directory [default: ./data]
     -g, --gzdir gzdir           Directory with the .gz files.
     -i, --indexdir INDEXDIR     Index directory [default: ./indexes].
-    -f, --file INFILE           File with the list of titles to search.
-                                '--continue')
+    -f, --file INFILE           File with the list of titles to search in the index.
     -l, --lang LANG             Language [default: en]
     -n, --dry-run               Dry run, only show the commands to be executed.
     -o, --outputdir OUTPUTDIR   Output directory [default: ./output]
     -p, --prefix PREFIX         Prefix to use for the output file name [default: pageviews]
-    --restart                   Restart the computation (opposite of
+    --restart                   Restart the computation.
     -y, --yearmonth YEARMONTH   Year and month to analyze, in the format
                                 YYYY-MM
     -h, --help                  Show this help message and exits.
@@ -200,6 +200,14 @@ gzfile=$(find "${GZDIR}/${YEARMONTH}" \
 gzdir_yearmonth=$(dirname "${gzfile}")
 
 indexfile="${indexdir}/${yearmonth}_index"
+simplified_redirects_file="./output/${lang}/${filename}.simplified-quoted-redirects.txt"
+
+if [ ! -f "$simplified_redirects_file" ]; then
+  (>&2 echo "ERROR: redirect file")
+  (>&2 echo "  $simplified_redirects_file" )
+  (>&2 echo "not found")
+  exit 1
+fi
 
 echodebug -ne "  * Build index for ${yearmonth} \t\t ... "
 wrap_run "build_index" "$SCRIPT_PATH/build_index.sh" -d \
@@ -218,7 +226,7 @@ outputfile="${outputdir}/$lang/${pagename}.${prefix}.${yearmonth}.txt.gz"
 # zgrep -E -f ./output/en/Zika_virus.quoted-redirects.txt \
 #             ./data/2016-05/part* | \
 #   gzip > ./output/en/Zika_virus.quoted-redirects.pageviews.2016-05.txt.gz
-wrap_run "extract" extract_data "$INFILE" "$datadir" "$yearmonth" "$outputfile"
+wrap_run "extract" extract_data "$simplified_redirects_file" "$datadir" "$yearmonth" "$outputfile"
 
 echodebug 'done!'
 
